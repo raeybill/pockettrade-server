65ad7067ea5dcbb6a3967d9efe86c25aedbf651d
Ræy Bill <109455914+raeybill@users.noreply.github.com>
2024-10-03 16:40:13 +0100

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,110 @@
+var express = require("express");
+const UsersDatabase = require("../models/User");
+const { hashPassword } = require("../utils");
+var router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  const users = await UsersDatabase.find();
+
+  res.status(200).json({ code: "Ok", data: users });
+});
+
+/* GET users listing. */
+router.get("/:email", async function (req, res, next) {
+  const { email } = req.params;
+
+  const user = await UsersDatabase.findOne({ email: email });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+
+  res.status(200).json({ code: "Ok", data: user });
+});
+router.delete("/:email/delete", async function (req, res, next) {
+  const { email } = req.params;
+
+  const user = await UsersDatabase.findOne({ email: email });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+
+  user.deleteOne();
+
+  res.status(200).json({ code: "Ok" });
+});
+
+router.put("/:_id/profile/update", async function (req, res, next) {
+  const { _id } = req.params;
+
+  const user = await UsersDatabase.findOne({ _id: _id });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+
+  try {
+    await user.update({
+      ...req.body,
+    });
+
+    return res.status(200).json({
+      message: "update was successful",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/:_id/accounts/update", async function (req, res, next) {
+  const { _id } = req.params;
+  const accountDict = req.body;
+  const data = accountDict.values;
+
+  const user = await UsersDatabase.findOne({ _id: _id });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+
+  const cummulative = Object.assign({}, user.accounts, JSON.parse(data));
+
+  console.log(cummulative);
+
+  try {
+    await user.updateOne({
+      accounts: {
+        ...cummulative,
+      },
+    });
+
+    return res.status(200).json({
+      message: "Account was updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:_id/accounts", async function (req, res, next) {
+  const { _id } = req.params;
+
+  const user = await UsersDatabase.findOne({ _id: _id });
+
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+
+  return res.status(200).json({
+    data: user.accounts,
+    message: "update was successful",
+  });
+});
+
+module.exports = router;
